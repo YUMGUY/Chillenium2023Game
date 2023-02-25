a2093a1c3f97d610cdc37a1065f0f239fa15c7fc
YUMGUY <timothytipham@gmail.com>
2023-02-24 21:37:43 -0600

    dialogue foundation need more

---
+++ Assets/DialogueSystem/MarriageConversation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewMarriageConvo", menuName = "Dialogue_Data/New Marriage Convo")]
+public class MarriageConversation : ScriptableObject
+{
+    [Header("scene's convo")]
+    public List<Conversation> conversations;
+
+
+    [System.Serializable]
+    public struct Conversation
+    {
+        [TextArea(2, 10)]
+        public string convoText;
+        public Sprite currentSpriteEmotion;
+        public SpeakerInfo character;
+        public Action currentAction;
+
+        [SerializeField]
+        public enum Action
+        {
+            IDLE, SHOW, DISAPPEAR
+        }
+    }
+}
--- Assets/DialogueSystem/SpeakerInfo.cs
+++ Assets/DialogueSystem/SpeakerInfo.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeakerInfo : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[CreateAssetMenu(fileName = "New Speaker", menuName = "Dialogue_Data/New Speaker")]
+public class SpeakerInfo : ScriptableObject
+{
+    public string speakerName;
+    public Color nameColor;
+    //potential text color associated with character
 }
